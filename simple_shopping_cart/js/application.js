8a7a644eb5453449ce1a9d7f0fcc3efdf6916071
JUSTIN SCOTT <JUSTINSCOTT820@GMAIL.COM>
2024-06-08 15:53:48 -0700

    grand total function finished

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -3,10 +3,20 @@ var updateSubTotal = function (ele) {
   var productQuantity = parseFloat($(ele).find('.quantity input').val());
   var productSubTotal = productPrice * productQuantity;
   $(ele).children('.productTotal').html('$' + productSubTotal);
+
+  return productSubTotal
 }
 
+var sum = function (acc, x) { return acc + x; };
 
-
+var grandTotal = function() {
+  var productTotals = [];
+  $('tbody tr').each(function(i, ele) {
+    productTotals.push(updateSubTotal(ele));
+  })
+  var cartTotal = productTotals.reduce(sum);
+  $('#grandTotal').html(cartTotal);
+}
 
 $(document).ready(function() {
   $('tbody tr').each(function() {
@@ -15,10 +25,11 @@ $(document).ready(function() {
 
   $('.btn.remove').on('click', function (event) {
     $(this).closest('tr').remove();
-    updateSubTotal();
+    grandTotal();
   });
 
   $(document).on('input', 'tr input', function () {
     updateSubTotal(this.closest('tr'));
+    grandTotal();
   });
 })
