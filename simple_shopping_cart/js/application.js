765932c103d61bc59ed7bb2f18a1c03d82d4e250
JUSTIN SCOTT <JUSTINSCOTT820@GMAIL.COM>
2024-06-13 12:24:38 -0700

    add new product structure and function

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -18,6 +18,8 @@ var grandTotal = function() {
   $('#grandTotal').html(cartTotal);
 }
 
+
+
 $(document).ready(function() {
   $('tbody tr').each(function() {
     updateSubTotal(this);
@@ -32,4 +34,28 @@ $(document).ready(function() {
     updateSubTotal(this.closest('tr'));
     grandTotal();
   });
+
+  $('#addProduct').on('submit', function(event) {
+    event.preventDefault();
+    var name = $(this).children('[name = name]').val();
+    var cost = $(this).children('[name = cost]').val();
+    var quantity = $(this).children('[name = quantity]').val();
+    console.log(name, cost, quantity);
+
+    $('tbody').append('<tr>' + 
+    '<td class = "name">' + name + '</td>' +
+    '<td class = "cost">' + cost + '</td>' +
+    '<td class = "quantity"><input type = "number" value = "' + quantity + '" /></td>' +
+    '<td class = "productTotal"></td>' +
+    '<td><button class = "btn btn-light btn-sm remove">remove</button></td>' +
+    '</tr>');
+
+    var newRow = $('tbody tr:last');
+    updateSubTotal(newRow);
+    grandTotal();
+    $(this).children('[name = name]').val('');
+    $(this).children('[name = cost]').val('');
+    $(this).children('[name = quantity]').val('');
+  })
+  
 })
