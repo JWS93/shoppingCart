2554fcef44503f083add97837cf058b011a94a31
JUSTIN SCOTT <JUSTINSCOTT820@GMAIL.COM>
2024-06-21 15:31:46 -0700

    final fix on submit form

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -1,6 +1,6 @@
 var updateSubTotal = function (ele) {
   var productPrice = parseFloat($(ele).children('.cost').text());
-  var productQuantity = parseFloat($(ele).find('.quantity input').val());
+  var productQuantity = parseFloat($(ele).find('.quantity input').val()) || 0;
   var productSubTotal = productPrice * productQuantity;
   $(ele).children('.productTotal').html('$' + productSubTotal);
 
@@ -25,7 +25,7 @@ $(document).ready(function() {
     updateSubTotal(this);
   });
 
-  $('.btn.remove').on('click', function (event) {
+  $(document).on('click', '.btn.remove', function (event) {
     $(this).closest('tr').remove();
     grandTotal();
   });
@@ -40,12 +40,19 @@ $(document).ready(function() {
     var name = $(this).children('[name = name]').val();
     var cost = $(this).children('[name = cost]').val();
     var quantity = $(this).children('[name = quantity]').val();
-    console.log(name, cost, quantity);
+
+    if (quantity === '') {
+      quantity = 0;
+    }
+    if (quantity < 0) {
+      quantity = 0;
+    }
+
 
     $('tbody').append('<tr>' + 
     '<td class = "name">' + name + '</td>' +
     '<td class = "cost">' + cost + '</td>' +
-    '<td class = "quantity"><input type = "number" value = "' + quantity + '" /></td>' +
+    '<td class = "quantity"><input type = "number" value = "' + quantity + '" min = "0" /></td>' +
     '<td class = "productTotal"></td>' +
     '<td><button class = "btn btn-light btn-sm remove">remove</button></td>' +
     '</tr>');
